81219a032682c35a6f256a931b872c8dc7f3578e
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-08-26 19:48:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,57 @@ from dotenv import load_dotenv
 import os
 
 
-def post_comic(vk_token, group_id, message, response_save_wall_photo):
+def get_wall_upload_server(vk_token, group_id):
+    payload = {'access_token': vk_token,
+               'v': '5.122',
+               'group_id': group_id}
 
+    url = 'https://api.vk.com/method/photos.getWallUploadServer'
+
+    response = requests.get(url, params=payload).json()
+    if 'error' in response:
+        raise requests.exceptions.HTTPError(response['error'])
+
+    upload_url = response['response']['upload_url']
+
+    with open('comic.png', 'rb') as file:
+        files = {'photo': file}
+
+        response_upload_photo = requests.post(upload_url, files=files).json()
+        if 'error' in response_upload_photo:
+            raise requests.exceptions.HTTPError(response_upload_photo['error'])
+        return response_upload_photo
+
+
+def save_wall_photo(vk_token, group_id):
+    photo_params = get_wall_upload_server(vk_token, group_id)
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    photo = photo_params['photo']
+    server = photo_params['server']
+    hash_photo = photo_params['hash']
+
+    payload = {'access_token': vk_token,
+               'v': '5.122',
+               'group_id': group_id,
+               'photo': photo,
+               'server': server,
+               'hash': hash_photo}
+
+    response = requests.post(url, params=payload).json()
+    if 'error' in response:
+        raise requests.exceptions.HTTPError(response['error'])
+
+    return response['response']
+
+
+def post_comic(vk_token, group_id, message):
+    comic_params = save_wall_photo(vk_token, group_id)
     url = 'https://api.vk.com/method/wall.post'
 
-    owner_id = response_save_wall_photo[0]['owner_id']
+    owner_id = comic_params[0]['owner_id']
     from_group = 1
     message = message
-    media_id = response_save_wall_photo[0]['id']
+    media_id = comic_params[0]['id']
 
     payload = {'access_token': vk_token,
                'v': '5.122',
@@ -20,48 +63,10 @@ def post_comic(vk_token, group_id, message, response_save_wall_photo):
                'attachments': f'photo{owner_id}_{media_id}',
                'owner_id': -group_id}
 
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-
-
-def save_wall_photo(vk_token, group_id, response_get_wall_upload_server):
-
-    url = 'https://api.vk.com/method/photos.saveWallPhoto'
-    photo = response_get_wall_upload_server['photo']
-    server = response_get_wall_upload_server['server']
-    hash_photo = response_get_wall_upload_server['hash']
-
-    payload = {'access_token': vk_token,
-               'v': '5.122',
-               'group_id': group_id,
-               'photo': photo,
-               'server': server,
-               'hash': hash_photo}
-    response = requests.post(url, params=payload)
-    response.raise_for_status()
-
-    return response.json()['response']
-
-
-def get_wall_upload_server(vk_token, group_id):
-    payload = {'access_token': vk_token,
-               'v': '5.122',
-               'group_id': group_id}
-
-    url = 'https://api.vk.com/method/photos.getWallUploadServer'
-
     response = requests.get(url, params=payload).json()
+    if 'error' in response:
+        raise requests.exceptions.HTTPError(response['error'])
 
-    upload_url = response['response']['upload_url']
-
-    with open('comic.png', 'rb') as file:
-        files = {
-            'photo': file
-        }
-        response = requests.post(upload_url, files=files)
-        response.raise_for_status()
-
-        return response.json()
 
 
 def download_comic(url):
@@ -97,12 +102,11 @@ def main():
     vk_token = os.getenv('VK_ACCESS_TOKEN')
 
     group_id = 198053823
-
-    message = get_comic_pic_and_message()
-
-    post_comic(vk_token, group_id, message,
-               save_wall_photo(vk_token, group_id,
-                               get_wall_upload_server(vk_token, group_id)))
+    try:
+        message = get_comic_pic_and_message()
+        post_comic(vk_token, group_id, message)
+    finally:
+        os.remove('comic.png')
 
 
 if __name__ == '__main__':
